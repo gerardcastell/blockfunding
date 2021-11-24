5691e008cefe4e924f1ad206a855f490ca901d7c
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-11-25 00:06:00 +0100

    [UPDATE] type fixing for launching app properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3';
+import { useState, useEffect } from 'react';
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 function App() {
+  const [accounts, setAccounts] = useState<string[]>([]);
+  if (typeof window.ethereum !== 'undefined') {
+    console.log('MetaMask is installed', window.ethereum);
+  }
+
+  useEffect(() => {
+    const web3 = new Web3('http://localhost:8545');
+
+    console.log('On mount');
+    async function getAccounts() {
+      const accs = await web3.eth.getAccounts();
+      setAccounts(accs);
+    }
+    getAccounts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h3>Account List:</h3>
+        {accounts?.map((acc, idx) => (
+          <p style={{ fontSize: '12px' }} key={idx}>
+            {acc}
+          </p>
+        ))}
       </header>
     </div>
   );
