30ca769c289e6d84b7e2d7c01a5889f20b0a082c
Gerard Castell <gerard.castell@cleverea.com>
2021-11-26 16:43:41 +0100

    [UPDATE] ask for metamask connection on init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import Web3 from 'web3';
 import './App.css';
 import { IAccount } from './components/IAccount';
 import TransactionForm from './components/TransactionForm';
-
+declare global {
+  interface Window {
+    web3: Web3;
+    ethereum: any;
+  }
+}
 function App() {
-
   const [accounts, setAccounts] = useState<IAccount[]>([]);
-  const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-
+  const [ethEnabled, setEthEnabled] = useState(false);
+  const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
   useEffect(() => {
     setAccounts([]);
     fetchData();
+    if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+      window.ethereum.enable();
+      setEthEnabled(true);
+    } else {
+      setEthEnabled(false);
+    }
+    if (!ethEnabled) {
+      alert('Please install MetaMask to use this dApp!');
+    }
   }, []);
-
 
   async function fetchData(): Promise<void> {
     const accountNumbers = await web3.eth.getAccounts();
     for (const accountNumber of accountNumbers) {
       const balance = await web3.eth.getBalance(accountNumber);
-      setAccounts(accounts => [...accounts, { address: accountNumber, balance}])
-    };
-  };
+      setAccounts((accounts) => [
+        ...accounts,
+        { address: accountNumber, balance },
+      ]);
+    }
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <TransactionForm web3={web3} accounts={accounts}/>
+    <div className='App'>
+      {accounts.map((acc, idx) => (
+        <p key='idx'>acc</p>
+      ))}
+      <header className='App-header'>
+        <TransactionForm web3={web3} accounts={accounts} />
       </header>
     </div>
-
   );
 }
 
--- src/components/TransactionForm.tsx
+++ src/components/TransactionForm.tsx
@@ -1,60 +1,79 @@
-import { useForm, SubmitHandler } from "react-hook-form";
-import Web3 from "web3";
-import { ethToWei, weiToEth } from "../shared/utils";
-import { IAccount } from "./IAccount";
+import { useForm, SubmitHandler } from 'react-hook-form';
+import Web3 from 'web3';
+import { ethToWei, weiToEth } from '../shared/utils';
+import { IAccount } from './IAccount';
 
 type Inputs = {
-    origin: string,
-    destination: string,
-    amount: string
+  origin: string;
+  destination: string;
+  amount: string;
 };
 
-export default function TransactionForm({ web3, accounts }: { web3: Web3, accounts: IAccount[] }) {
-    const { register, handleSubmit } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = async data => {
-        try {
-            const receipt = await web3.eth.sendTransaction({
-                from: data.origin,
-                to: data.destination,
-                value: ethToWei(data.amount)
-                // gas?: number | string;
-                // gasPrice?: number | string | BN;
-                // data?: string;
-                // nonce?: number;
-                // chainId?: number;
-                // common?: Common;
-                // chain?: string;
-                // hardfork?: string;
-            });
-            receipt.status && alert("Transfer done");
-        } catch (error) {
-            alert(error);
-        }
-    };
+export default function TransactionForm({
+  web3,
+  accounts,
+}: {
+  web3: Web3 | undefined;
+  accounts: IAccount[];
+}) {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      const receipt = await web3?.eth.sendTransaction({
+        from: data.origin,
+        to: data.destination,
+        value: ethToWei(data.amount),
+        // gas?: number | string;
+        // gasPrice?: number | string | BN;
+        // data?: string;
+        // nonce?: number;
+        // chainId?: number;
+        // common?: Common;
+        // chain?: string;
+        // hardfork?: string;
+      });
+      receipt?.status && alert('Transfer done');
+    } catch (error) {
+      alert(error);
+    }
+  };
 
-    return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-        <form onSubmit={handleSubmit(onSubmit)} key="origin">
-            <select {...register("origin")}>
-                <option value=""></option>
-                {
-                    accounts.filter(account => account.address).map((account) => {
-                        return <option value={account.address}>{`${account.address} (${weiToEth(account.balance)} ETH)`}</option>
-                    })
-                }
-            </select>
-            <br />
-            <select {...register("destination")} key="destination">
-                <option value=""></option>
-                {
-                    accounts.filter(account => account.address !== origin).map((account) => {
-                        return <option value={account.address}>{`${account.address} (${weiToEth(account.balance)} ETH)`}</option>
-                    })
-                }
-            </select>
-            <br />
-            <input {...register("amount")} placeholder="Amount" autoComplete="false" key="amount"/>
-            <input type="submit" />
-        </form>
-    );
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)} key='origin'>
+      <select {...register('origin')}>
+        <option value=''></option>
+        {accounts
+          .filter((account) => account.address)
+          .map((account) => {
+            return (
+              <option value={account.address}>{`${account.address} (${weiToEth(
+                account.balance
+              )} ETH)`}</option>
+            );
+          })}
+      </select>
+      <br />
+      <select {...register('destination')} key='destination'>
+        <option value=''></option>
+        {accounts
+          .filter((account) => account.address !== origin)
+          .map((account) => {
+            return (
+              <option value={account.address}>{`${account.address} (${weiToEth(
+                account.balance
+              )} ETH)`}</option>
+            );
+          })}
+      </select>
+      <br />
+      <input
+        {...register('amount')}
+        placeholder='Amount'
+        autoComplete='false'
+        key='amount'
+      />
+      <input type='submit' />
+    </form>
+  );
 }
