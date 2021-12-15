aa3590202d8ab0906b9ebb21dab7d0df1c28394c
Sergio Gimenez <sergiogimenezanton@gmail.com>
2021-12-15 22:18:56 +0100

    First method implementations

--- smart_contract/contract.sol
+++ smart_contract/contract.sol
@@ -3,18 +3,21 @@
 pragma solidity >=0.8.10;
 
 contract CrowdFunding {
-    address private owner;
+    address public owner;
     uint256 public balance;
     uint256 public ethGoal;
     uint256 public deadline;
     mapping(address => uint256) public balanceReceived;
-    mapping(address => mapping(uint256 => bool)) public isIndexClaimed; // Amount of ETH each org can claim per claimable event
+    mapping(address => mapping(uint256 => bool)) public isDonationClaimed; // Amount of ETH each org can claim per claimable event
 
-
-    constructor(uint8 _days) {
+    constructor(uint16 _days, uint256 _ethGoal) {
         // msg.sender is the address of the person who deployed the SC
         owner = msg.sender;
-        deadline = block.timestamp + _days * 24 * 3600;
+        // deadline = block.timestamp + _days * 24 * 3600; // TODO why is not working?
+        deadline = 100000000000000000000;
+
+        // Save the amount of ETH the person wants to raise in ether
+        ethGoal = _ethGoal * 1000000000000000000;
     }
 
     // modifier to check if the owner still accepts donations
@@ -33,57 +36,44 @@ contract CrowdFunding {
         require(balance >= ethGoal, "Goal is not achieved");
         _;
     }
-    
-    function setNewDeadline(uint256 _end) public isOwner {
-        // TODO
-    }
 
-    function makeDonation() public payable inTime {
+    // function makeDonation() public payable inTime {
+    function makeDonation() public payable {
+        require(balance <= ethGoal, "Goal is achieved");
+
         assert(msg.value == uint64(msg.value));
 
-        if (msg.value + this.balance > ethGoal) {
-            uint64 amountToDonate = ethGoal - this.balance;
-            uint64 amountToTxBack = msg.value - amountToDonate;
-            
-            // send back the amount to the sender
-            msg.sender.transfer(amountToTxBack);
-
-            // send the rest to the owner
-            address(this).transfer(amountToDonate);
-            balanceReceived[msg.sender] += msg.amountToDonate;
-        }
-        else {
-            // send the amount to the owner
-            address(this).transfer(msg.value);
-            balanceReceived[msg.sender] += msg.value;
-        }
+        // User sends donation to the contract
+        balanceReceived[msg.sender] += msg.value;
+        balance += msg.value;
 
         assert(balanceReceived[msg.sender] >= uint64(msg.value));
-    }   
-
-        function claim(uint256 _claimIndex) public {            
-            if (isIndexClaimed[msg.sender][_claimIndex] == false) {
-                isIndexClaimed[msg.sender][_claimIndex] = true;  // Org cannot claim multiple times per index
-                msg.sender.transfer(claimAmount[_claimIndex]);
-            }
-        }
-
-
-// TODO SC queda bloquejat quan el deadline ha expirat
-
-    function withdrawFunds(address payable _to) public isOwner isAchieved {           
-        _to.transfer(address(this).balance);
     }
-    
 
-    function retrieve() public isOwner returns (uint256) {
-        
-        require(address(this).balance < ethGoal, "Goal is achieved");
-        require (block.timestamp <= deadline, "Donations are open");
+    // TODO not working, everyone can claim
+    function claim(uint256 _amount) public {
+        // Check if the user has already claimed his funds
+        require(
+            isDonationClaimed[msg.sender][_amount] == false,
+            "Donation already claimed"
+        );
 
-        // TODO 
+        // TODO If the donator makes more donations??
+        // --> We could update the mapping with the new amount + the previous
 
+        // TODO If the donator selects a lower amount than the one he has donated?
 
-        return number;
+        // Mark that that user with this amount has already claimed funds
+        isDonationClaimed[msg.sender][_amount] = true;
+        balance -= _amount;
+
+        // Send funds to the donator
+        payable(msg.sender).transfer(_amount);
+    }
+
+    // TODO SC queda bloquejat quan el deadline ha expirat
+
+    function withdrawFunds(address payable _to) public isOwner isAchieved {
+        _to.transfer(address(this).balance);
     }
 }
