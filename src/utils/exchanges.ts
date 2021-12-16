01eef51069528e2734ba963aec8761788a96932d
martiriera <martiriera13@gmail.com>
2021-12-16 21:48:41 +0100

    [UPDATE] Exchange accepts both number or string

--- src/utils/exchanges.ts
+++ src/utils/exchanges.ts
@@ -1,7 +1,15 @@
-export function weiToEth(amount: string): number {
-    return (parseFloat(amount) / Math.pow(10, 18));
+export function weiToEth(amount: string | number): string | number {
+    if (typeof amount === "string"){ 
+        return (parseFloat(amount) / Math.pow(10, 18)).toString();
+    } else {
+        return amount / Math.pow(10, 18)
+    }
 };
 
-export function ethToWei(amount: string): number {
-    return (parseFloat(amount) * Math.pow(10, 18));
+export function ethToWei(amount: string | number): string | number {
+    if (typeof amount === "string"){ 
+        return (parseFloat(amount) * Math.pow(10, 18)).toString();
+    } else {
+        return amount * Math.pow(10, 18)
+    }
 };
