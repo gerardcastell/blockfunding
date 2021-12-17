02e1fd3c0eae7d34aac19c4f8815aabf17427906
Gerard Castell <gerard.castell@cleverea.com>
2021-12-17 12:26:15 +0100

    [UPDATED] added withdraw and claim

--- smart-contract/CrowdFunding.sol
+++ smart-contract/CrowdFunding.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
 
-pragma solidity >=0.8.7 <0.9.0;
-
+/**
+ * @title CrowdFunding
+ * @dev create and manage crowdfundings
+ */
 contract CrowdFunding {
     struct Project {
         // Leader of the crowdfunding campaign
         address owner;
+        // Title of the crowdfunding
+        string title;
         // The amount of wei raised so far
         uint256 balance;
         // Amount of founds the project founder wants to collect
@@ -14,86 +19,121 @@ contract CrowdFunding {
         // Deadline in seconds (unix timestamp)
         uint256 deadline;
         // Amount that every user has contributed
-        mapping(address => uint256) balanceReceived;
+        //mapping(address => uint256) balanceReceived;
         // Amount of ETH each org can claim per claimable event
-        mapping(address => mapping(uint256 => bool)) isDonationClaimed;
+        //mapping(address => mapping(uint256 => bool)) isDonationClaimed;
     }
 
-    // Mapping of crowdfunding address to the its Project struct
-    mapping(address => Project) public projects;
-    // List of crowdfunding projects
-    address[] public crowdfundingList;
+    uint256 number;
+    Project[] projects;
+    mapping (address => uint256) addressMap;
+    mapping (address => mapping(address => uint256)) donationLedger;
 
-    modifier inTime(address _crowdFundingAddress) {
-        require(block.timestamp <= projects[_crowdFundingAddress].deadline, "Donations are closed");
+     modifier inTime(address _crowdFundingAddress) {
+        uint256 idx = getIndexByAddress(_crowdFundingAddress);
+        require(block.timestamp <= projects[idx].deadline, "Donations are closed");
         _;
     }
 
     modifier notInTime(address _crowdFundingAddress) {
-        require(block.timestamp > projects[_crowdFundingAddress].deadline, "Donations are still ongoing");
+        uint256 idx = getIndexByAddress(_crowdFundingAddress);
+        require(block.timestamp > projects[idx].deadline, "Donations are still ongoing");
         _;
     }
 
     modifier isOwner(address _ownerAddr) {
-        require(msg.sender == projects[_ownerAddr].owner, "Caller is not the founder");
+        uint256 idx = getIndexByAddress(_ownerAddr);
+        require(msg.sender == projects[idx].owner, "Caller is not the founder");
         _;
     }
 
     modifier isNotOwner(address _ownerAddr) {
-        require(msg.sender != projects[_ownerAddr].owner, "Caller is the founder");
+        uint256 idx = getIndexByAddress(_ownerAddr);
+        require(msg.sender != projects[idx].owner, "Caller is the founder");
         _;
     }
 
     modifier isAchieved(address _crowdFundingAddress) {
-        require(projects[_crowdFundingAddress].balance >= projects[_crowdFundingAddress].ethGoal, "Goal is not achieved");
+        uint256 idx = getIndexByAddress(_crowdFundingAddress);
+        require(projects[idx].balance >= projects[idx].ethGoal, "Goal is not achieved");
         _;
     }
 
     modifier isNotAchieved(address _crowdFundingAddress) {
-        require(projects[_crowdFundingAddress].balance < projects[_crowdFundingAddress].ethGoal, "Goal is achieved");
+        uint256 idx = getIndexByAddress(_crowdFundingAddress);
+        require(projects[idx].balance < projects[idx].ethGoal, "Goal is achieved");
         _;
     }
 
-    function createNewProject(uint256 _segons, uint256 _ethGoal) public {
-        projects[msg.sender].owner = msg.sender;
-        projects[msg.sender].balance = 0;
-        projects[msg.sender].ethGoal =  _ethGoal * 1000000000000000000;
-        projects[msg.sender].deadline =  block.timestamp + _segons;
-
-        crowdfundingList.push(msg.sender);
-        assert(projects[msg.sender].owner == msg.sender);
-        assert(projects[msg.sender].balance == 0);
-        assert(projects[msg.sender].ethGoal ==  _ethGoal * 1000000000000000000);
-        assert(projects[msg.sender].deadline ==  block.timestamp + _segons);
+    //PRIVATE FUNCTIONS
+    function removeByIndex(uint i) private{
+        while (i<projects.length-1) {
+            projects[i] = projects[i+1];
+            i++;
+        }
     }
 
-    function makeDonation(address _crowdFundingAddress) public payable isNotOwner(msg.sender) inTime(_crowdFundingAddress) isNotAchieved(_crowdFundingAddress) {
-        projects[_crowdFundingAddress].balanceReceived[msg.sender] += msg.value;
-        projects[_crowdFundingAddress].isDonationClaimed[msg.sender][msg.value] == false;
-        projects[_crowdFundingAddress].balance += msg.value;
+    function getIndexByAddress(address _address) private view returns(uint256){
+        return addressMap[_address];
+    }
+        
+    function deleteProject(address _address) private {
+        uint256 idx = addressMap[_address];
+        if (idx >= projects.length) return;
+        for (uint i = idx; i<projects.length-1; i++){
+            projects[i] = projects[i+1];
+        }
+        delete projects[projects.length-1];
+    }
+
+    //PUBLIC FUNCTIONS
+    function createProject(string memory _title, uint256 _ethGoal, uint256 _seconds) public {
+        Project storage newProject = projects.push();
+        //Project({owner:msg.sender,balance:0, ethGoal:_ethGoal, deadline: block.timestamp + _seconds}); 
+        //projects.push(newProject);
+
+        newProject.owner = msg.sender;
+        newProject.balance = 0;
+        newProject.title = _title;
+        newProject.ethGoal = _ethGoal * (1 ether);
+        newProject.deadline = block.timestamp + _seconds;
+         
+        addressMap[msg.sender]=projects.length -1;
+    }
+
+    function makeDonation(address _crowdFundingAddress) public payable isNotOwner(msg.sender) inTime(_crowdFundingAddress) isNotAchieved(_crowdFundingAddress){
+        uint256 idx = getIndexByAddress(_crowdFundingAddress);
+     
+        projects[idx].balance += msg.value;
+        donationLedger[msg.sender][_crowdFundingAddress] += msg.value;
+    }
+
+
+    function getProjects() public view returns(Project[] memory){
+        return projects;
+    }
+
+    function getProject(address _address) public view returns(Project memory){
+        uint256 idx = addressMap[_address];
+        return projects[idx];
     }
 
     function claim(address _crowdFundingAddress, uint256 _amount) public notInTime(_crowdFundingAddress) isNotAchieved(_crowdFundingAddress) {
         // Check if the user has already claimed his funds
-       require(
-           projects[_crowdFundingAddress].isDonationClaimed[msg.sender][_amount] == false,
+        uint256 idx = getIndexByAddress(_crowdFundingAddress);
+        require(
+           donationLedger[msg.sender][_crowdFundingAddress] > 0,
            "Donation already claimed"
-       );
+        );
 
-        _amount = _amount * 1000000000000000000; // Convert to wei
+        _amount = _amount * (1 ether); // Convert to wei
        require(
-           projects[_crowdFundingAddress].balanceReceived[msg.sender] - _amount >= 0,
+            donationLedger[msg.sender][_crowdFundingAddress] - _amount >= 0,
            "Not enough funds"
        );
 
-       projects[_crowdFundingAddress].balanceReceived[msg.sender] -= _amount;
-
-       if (projects[_crowdFundingAddress].balanceReceived[msg.sender] == 0) {
-            // Mark that that user with this amount has already claimed funds
-           projects[_crowdFundingAddress].isDonationClaimed[msg.sender][_amount] = true;
-       }
-
-        projects[_crowdFundingAddress].balance -= _amount;
+        donationLedger[msg.sender][_crowdFundingAddress] -= _amount;
+        projects[idx].balance -= _amount;
 
         // Send funds to the donator address
         payable(msg.sender).transfer(_amount);
@@ -102,4 +142,5 @@ contract CrowdFunding {
     function withdrawFunds() public isOwner(msg.sender) isAchieved(msg.sender) {
         payable(msg.sender).transfer(address(this).balance);       
     }
+ 
 }
